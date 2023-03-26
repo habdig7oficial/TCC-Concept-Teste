16fcb4d9b1ac33fc4b4ef2d80da5370e7daa52d3
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2023-03-26 22:58:41 +0000

    LICENSE COPYLEFT NOTICE

--- go_gin/routes/index.go
+++ go_gin/routes/index.go
@@ -1,3 +1,18 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 package routes
 
 import (
@@ -55,10 +70,16 @@ func Hello(ctx *gin.Context){
 
 	params := ctx.Request.URL.Query().Get("hello")
 
+	license_notice := "This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>."
+
 	ctx.JSON(200, gin.H{
 		"message": "pong",
-		"params": params,
+		"license": "GNU AGPL v3",
+		"Free Software": "free as in freedom",
+		"Author": "Mateus Felipe da Silveira Vieira",
+		"COPYLEFT": "Copyright © 2023 Mateus Felipe da Silveira Vieira",
+		"license_notice": license_notice,
+		"query_params": params,
 		"dbvalue": found,
-		//"dbquery": dbquery,
 	})
 }
