354a89075e6fba2e8b8483b2168aba37782ed25f
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2023-03-27 02:48:51 +0000

    CONFIG Select func

---
+++ go_gin/config/utilities/Select.go
@@ -0,0 +1,80 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+package config
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type dbstruct struct {
+    ID     int8
+    TESTE	bool
+    RAMDOM	float64
+}
+
+
+
+func SelectAll(db *sql.DB, cols []string, where string, order_by string, IsASC bool)  (found []dbstruct) {
+
+    columns:= cols
+
+    var sort_query string
+    if IsASC == true{
+        sort_query = "ASC"
+    }else{
+        sort_query = "DESC"
+    }
+
+    fmt.Print(columns)
+
+    var mountedQuery string
+    for i := 0; i < len(columns); i++{
+        if i < len(columns) - 1{
+            mountedQuery =  fmt.Sprint(mountedQuery, columns[i], ", ")
+        } else{
+            mountedQuery =  fmt.Sprint(mountedQuery, columns[i])
+        }
+        fmt.Print(i)
+    }
+
+    fmt.Print(mountedQuery+"\n")
+
+    //a := fmt.Sprintf
+
+    read, err := db.Query("SELECT $1 FROM $2 ORDER BY $3 $4;", mountedQuery, where, order_by, sort_query)
+    if err != nil {
+        fmt.Print(err)
+		return found
+    }
+    defer read.Close()
+    // Loop through rows, using Scan to assign column data to struct fields.
+    for read.Next() {
+        var row dbstruct
+        if err := read.Scan(&row.ID, &row.TESTE, &row.RAMDOM); err != nil {
+            fmt.Print(err)
+			return found
+        }
+        found = append(found, row)
+    }
+    if err := read.Err(); err != nil {
+        fmt.Print(err)
+		return found
+    }
+    fmt.Print(found)
+
+	return found
+}
--- go_gin/routes/index.go
+++ go_gin/routes/index.go
@@ -1,24 +1,25 @@
 /*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>
 */
 
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"fmt"
-	"go_gin/config"	
+	"go_gin/config"
+	"go_gin/config/utilities"
+	"github.com/gin-gonic/gin"
 )
 
 func Hello(ctx *gin.Context){
@@ -30,37 +31,12 @@ func Hello(ctx *gin.Context){
 		VALUES( 2, false, 93);`)
 	fmt.Print("\n\ninsert - ", insert, "\n\n")
 
-	type dbstruct struct {
-		ID     int8
-		TESTE	bool
-		RAMDOM	float64
-	}
 	
-
-    var found []dbstruct
 	id := 2
 
-    read, err := postgres.Query("SELECT * FROM teste")
-    if err != nil {
-        fmt.Print(err)
-		return
-    }
-    defer read.Close()
-    // Loop through rows, using Scan to assign column data to struct fields.
-    for read.Next() {
-        var row dbstruct
-        if err := read.Scan(&row.ID, &row.TESTE, &row.RAMDOM); err != nil {
-            fmt.Print(err)
-			return
-        }
-        found = append(found, row)
-    }
-    if err := read.Err(); err != nil {
-        fmt.Print(err)
-		return
-    }
-    fmt.Print(found)
+	columns := []string{"*"}
 
+	found := config.SelectAll(postgres, columns, "teste", "id", true)
 
 	update := postgres.QueryRow(`UPDATE teste SET teste=true WHERE id=$1`, id)
 	fmt.Print("\n\nupdate - ", update, "\n\n")
